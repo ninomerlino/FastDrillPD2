e1cde85090036527b75c3eb561ed3ac064639d40
ninomerlino <55147372+ninomerlino@users.noreply.github.com>
2021-12-30 19:53:51 +0100

    Add files via upload
    added chat message when someone joins the a lobby with fast drill activated
    added sync for timergui of other peers

--- FastDrill/main.lua
+++ FastDrill/main.lua
@@ -1,23 +1,38 @@
-
-
+--modiefies the drill speed when the host starts it
 Hooks:PostHook(TimerGui, "set_timer_multiplier", "fastdrillStart", function(self, ...)
-	if not self then log("no self") end
-	if not self._timer_multiplier  then log("no timer mult") end
-	log("Entered hook2")
-	log(tostring(self._timer_multiplier))
-	log(tostring(self))
-	if managers.network and managers.network:session() then
+	log("Entered hook")
+	if Network:is_server() and self and self._timer_multiplier then 
+		local multi
 		if managers.groupai and managers.groupai:state() and managers.groupai:state():whisper_mode() then
-			log("is wisper mode--------------------------------------------")
-			self._timer_multiplier = 0.1
+			multi = 0.1
 		else
-			log("is loud mode--------------------------------------------")
-			self._timer_multiplier = 0.2
+			multi = 0.2
+		end
+		if self._timer and managers.network:session() and self._unit then
+			managers.network:session():send_to_peers_synched("start_timer_gui", self._unit, self._timer)
+		else
+			log("no timer")
 		end
 	else
-		log("player is not host")
+		log("----------------------Server----------------------")
+		log(tostring(Network:is_server()))
+		log("----------------------Self----------------------")
+		if self then log(tostring(self)) else log("No self") end
+		log("----------------------Multi----------------------")
+		if self._timer_multiplier then log(tostring(self._timer_multiplier)) else log("No time multiplier") end
 	end
 end)
-
-
+--send message to people who join the lobby
+Hooks:PostHook(NetworkManager, "on_peer_added", "fastdrillJoinLobby", function(self, peer, peer_id)
+	if Network:is_server() then
+		DelayedCalls:Add("FDcomunication", 2, function()
+			log("Deleyed call started")
+			local message = "The host has FastDrill active!"
+			local targetpeer = managers.network:session() and managers.network:session():peer(peer_id)
+			if targetpeer then
+				targetpeer:send("send_chat_message", ChatManager.GAME, message)
+			end
+		end)
+	end
+end)
 
